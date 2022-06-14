dd895b62f19fe2c3dd615a31db5ea7decfa281f5
Michael Rocha <Fiscal21@center.local>
2022-06-14 18:11:42 -0300

    message

--- testedjango/settings.py
+++ testedjango/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '0.0.0.0',
-    'testetecnicoback.herokuapp.com', # your herokuapp url
+    'testapimichael.herokuapp.com', # your herokuapp url
     '127.0.0.1'
 ]
 
