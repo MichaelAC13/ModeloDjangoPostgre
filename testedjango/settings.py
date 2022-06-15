a01c98de5bb01382c48df0c284c6f9931c3ee03d
Michael Rocha <Fiscal21@center.local>
2022-06-15 08:48:11 -0300

    make it better

--- testedjango/settings.py
+++ testedjango/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from threading import local
-import django_heroku
-import django_on_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,7 +37,6 @@ REST_FRAMEWORK = {
         'rest_framework.parsers.JSONParser',
     )
 }
-
 
 # Application definition
 
@@ -146,5 +144,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-django_heroku.settings(locals())
-django_on_heroku.settings(locals())
